151f4a949c9d26b3ccccb8b5ff6ecf85f4d2a783
Brian Steele <brsteele2123@gmail.com>
2019-02-25 09:40:54 -0800

    building in responses to actual API calls

--- api-simulator/server.js
+++ api-simulator/server.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const test = require('./test');
 const port = 9011;
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/about', function(req, res) {
-  res.send('about it');
+app.get('/', (req, res) => {
+  res.status(404).send('Must provide username in URL');
+});
+
+app.get('/:username', (req, res) => {
+  const username = req.params.username;
+  if (!username) {
+    res.status(401).send('bad request');
+  }
+  fs.readFile(`data/${username}.json`, (err, rawData) => {
+    const dataToSend = JSON.parse(rawData);
+    res.send(dataToSend);
+  });
 });
 
 app.use('/seinfeld', test);
